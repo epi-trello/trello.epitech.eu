6aac38683770b6dff432469cb4e9cc88c3a10323
David Abou <72009265+davidabou@users.noreply.github.com>
2026-01-12 17:56:45 +0100

    feat: add login page (#31)
    * feat: add login page
    
    * fix: redirection when user already logged in
    
    * fix: restore precedent version and add description to error toast
    
    * chore: apply automated updates
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -31,7 +31,7 @@ declare module 'vue-router' {
 
 export default defineNuxtRouteMiddleware(async (to) => {
   // If auth is disabled, skip middleware
-  if (to.meta?.auth === false || to.path === '/board' || to.path === '/tableaux' || to.path === '/modeles' || to.path === '/accueil') {
+  if (to.meta?.auth === false) {
     return
   }
   const { loggedIn, options, fetchSession } = useAuth()
